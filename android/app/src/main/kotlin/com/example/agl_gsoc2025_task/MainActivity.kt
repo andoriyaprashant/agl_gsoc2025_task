bd5ccf446b7260f8093fb9ca7632f7b1d6d40b38
andoriyaprashant <prashantandoriya@gmail.com>
2025-03-23 03:47:20 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/agl_gsoc2025_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.agl_gsoc2025_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("AGL GSoC App")),
+        body: const HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showImage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Prashant Andoriya",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                showImage = !showImage;
+              });
+            },
+            child: const Text("Show Image"),
+          ),
+          const SizedBox(height: 20),
+          if (showImage)
+            Image.asset(
+              'assets/agl_image.webp',
+              width: 200,
+            ),
+        ],
+      ),
+    );
+  }
+}
